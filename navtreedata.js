0dcf4faff015df81888baa83a00636694828af1b
sblondon <sblondon@users.noreply.github.com>
2025-10-30 13:11:18 +0000

    deploy: 6c09d354398abc6523c165c57f83bc09eee8fc61

---
+++ navtreedata.js
@@ -0,0 +1,96 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PSPSDK", "index.html", [
+    [ "PSP Software Development Kit", "index.html", "index" ],
+    [ "Contributor Covenant Code of Conduct", "md__2github_2workspace_2CODE__OF__CONDUCT.html", [
+      [ "Our Pledge", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md19", null ],
+      [ "Our Standards", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md20", null ],
+      [ "Enforcement Responsibilities", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md21", null ],
+      [ "Scope", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md22", null ],
+      [ "Enforcement", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md23", null ],
+      [ "Enforcement Guidelines", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md24", [
+        [ "Correction", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md25", null ],
+        [ "Warning", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md26", null ],
+        [ "Temporary Ban", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md27", null ],
+        [ "Permanent Ban", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md28", null ]
+      ] ],
+      [ "Attribution", "md__2github_2workspace_2CODE__OF__CONDUCT.html#autotoc_md29", null ]
+    ] ],
+    [ "Topics", "topics.html", "topics" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", "globals_type" ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ],
+    [ "Examples", "examples.html", "examples" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"With-example.html",
+"gdb-kernellib_8c.html#a26c32508b1b70f048db8e1da4fd1edc1",
+"group__Debug.html#ga1b37cb52375a34f9980654d4a7b5ac3d",
+"group__IntrManKern.html#ga11bc0003d68cf0601102aab873a94e50",
+"group__ThreadMan.html#ga5b67259973b010ebe909904781ac737d",
+"guInternal_8h.html#a52cd96c491c3ee2073085e7d141404f2",
+"guInternal_8h.html#ac698ab9d6cbd8d7ad321e50d4cb98016",
+"prof_8c.html#aa2b6dc715c3be9530dadea56e25c6089",
+"pspexception_8h.html",
+"pspgu_8h.html#a2c3dcea74888268f1ac0b8ad0053b2ea",
+"psphttp_8h.html#a0428fbbd34fd57440460e78a63e6d4f7",
+"pspkermit_8h.html#a5c9fcad41780b40a9af2bbd6ee41b7ef",
+"pspkerror_8h.html#ada38ce7dd0ddff755524e736afd6fcacabd26d36cd440070dad2737afc1f8482f",
+"pspnet__adhocmatching_8h.html#a0ebb2e6e6c8c64df91945a5b862e57aba903d1dce6bb558954f811f187845717a",
+"pspsysevent_8h.html#afa5b1b5380d375f378b9f44bfc308545",
+"pspusbstor_8h.html#ac90fd519aaff8d8cf63f14c174a797ab",
+"psputility__savedata_8h.html#a621499b8c423dfe72bfcb203c501d26b",
+"sceGuTexOffset_8c.html",
+"structGuSettings.html#a199b3c3fa9ecf62b48c67bfd21e62830",
+"structSceKernelAlarmInfo.html",
+"structSceModule.html#a278fb22ee7708b220956cd9b439c0137",
+"structSceSCContext.html",
+"struct__SceUtilityOskData.html#a735823e4d85b07939c201c56d6889c3c",
+"structsw__breakpoint.html#a9f93437f839f537571de73da8df3909d"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
